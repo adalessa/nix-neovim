299cf2e534023956c0bcc47c20adacdffe256184
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-10-26 22:48:43 +0200

    Update lualine

--- config/lualine.nix
+++ config/lualine.nix
@@ -1,16 +1,50 @@
+{pkgs, ...}:
 {
-  plugins.lualine = {
-    enable = true;
-    globalstatus = true;
-    extensions = [ "quickfix" "fugitive" ];
-    winbar.lualine_c = [ "filename" ];
-    inactiveWinbar.lualine_c = [ "filename" ];
-    sections.lualine_c = [{
-      name = "filename";
-      extraConfig = {
-        file_status = true;
-        path = 1;
-      };
-    }];
-  };
+  extraPlugins = [pkgs.vimPlugins.lualine-nvim];
+
+  extraConfigLua = ''
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        icons_enabled = true,
+        globalstatus = true,
+      },
+      extensions = { "quickfix" },
+      sections = {
+        lualine_a = { { "mode", upper = true } },
+        lualine_b = { { "branch", icon = "" }, "db_ui#statusline" },
+        lualine_c = { { "filename", file_status = true, path = 1 } },
+        lualine_x = {
+          "diagnostics",
+          "diff",
+          {
+            "require('laravel.status').get('php')",
+            icon = {" ", color = { fg = "5e79be" }, },
+          },
+          {
+            "require('laravel.status').get('laravel')",
+            icon = {" ", color = { fg = "f9322c" }, },
+          },
+        },
+        lualine_y = { "filetype" },
+        lualine_z = { "location" },
+      },
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+      inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  '';
 }
