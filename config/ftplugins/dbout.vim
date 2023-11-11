07acc2d3478ceb7c658002a0ca534dc42fcefc75
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-11-11 10:15:02 +0100

    feat: ftplugins

---
+++ config/ftplugins/dbout.vim
@@ -0,0 +1,2 @@
+set nofoldenable
+set colorcolumn=
--- config/ftplugins/default.nix
+++ config/ftplugins/default.nix
@@ -1,5 +1,10 @@
 {
   extraFiles = {
+    "ftplugin/cucumber.lua" = builtins.readFile ./cucumber.lua;
+    "ftplugin/dbout.vim" = builtins.readFile ./dbout.vim;
     "ftplugin/go.lua" = builtins.readFile ./go.lua;
+    "ftplugin/json.vim" = builtins.readFile ./json.vim;
+    "ftplugin/nix.lua" = builtins.readFile ./nix.lua;
+    "ftplugin/rust.lua" = builtins.readFile ./rust.lua;
   };
 }
