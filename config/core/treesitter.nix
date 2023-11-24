e9dcbdf907c2f4c3e6612e001840cba0d91884b4
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-11-24 19:25:43 +0100

    style: group into one object

--- config/core/treesitter.nix
+++ config/core/treesitter.nix
@@ -1,9 +1,11 @@
 {pkgs, ...}: {
-  plugins.treesitter = {
-    enable = true;
-    indent = true;
-    grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
-  };
+  plugins = {
+    treesitter = {
+      enable = true;
+      indent = true;
+      grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
+    };
 
-  plugins.treesitter-context.enable = true;
+    treesitter-context.enable = true;
+  };
 }
