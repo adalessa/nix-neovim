0ab7c570c8ac1b3a25264867660fd7be0b7e84b8
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-12-09 15:33:46 +0100

    feat: add treesitter-textobjects

--- config/core/treesitter.nix
+++ config/core/treesitter.nix
@@ -7,5 +7,15 @@
     };
 
     treesitter-context.enable = true;
+    treesitter-textobjects = {
+      enable = true;
+      select = {
+        enable = true;
+        keymaps = {
+          "af" = "@function.outer";
+          "if" = "@function.inner";
+        };
+      };
+    };
   };
 }
