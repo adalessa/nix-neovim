8fdb7eb721509aa7b64e42c82cc4ee211f30df6b
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-12-18 21:18:22 +0100

    feat: improve keymaps for floaterm

--- config/modules/terminal/default.nix
+++ config/modules/terminal/default.nix
@@ -12,9 +12,27 @@
     }
     {
       mode = "t";
-      key = "<c-t>";
+      key = "<c-q>";
       options.desc = "";
       action = "<c-\\><c-n>:FloatermToggle<cr>";
+    }
+    {
+      mode = "t";
+      key = "<c-t>";
+      options.desc = "";
+      action = "<c-\\><c-n>:FloatermNew<cr>";
+    }
+    {
+      mode = "t";
+      key = "<c-n>";
+      options.desc = "";
+      action = "<c-\\><c-n>:FloatermNext<cr>";
+    }
+    {
+      mode = "t";
+      key = "<c-p>";
+      options.desc = "";
+      action = "<c-\\><c-n>:FloatermPrev<cr>";
     }
   ];
 }
