2bea401832bc0b58ab6ab03220d0b97177dc86fd
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2023-12-22 11:14:33 +0100

    feat: improve keymaps for terminal

--- config/modules/terminal/default.nix
+++ config/modules/terminal/default.nix
@@ -3,42 +3,30 @@
     enable = true;
   };
 
+  autoCmd = [
+    {
+      event = ["FileType"];
+      pattern = ["floaterm"];
+      callback = {
+        __raw = ''
+          function (event)
+            vim.keymap.set({"n", "t"}, "<c-h>", "<Cmd>FloatermHide<cr>", {buffer = event.buf})
+            vim.keymap.set({"n", "t"}, "<c-w>", "<Cmd>FloatermKill<cr>", {buffer = event.buf})
+            vim.keymap.set({"n", "t"}, "<c-t>", "<Cmd>FloatermNew<cr>", {buffer = event.buf})
+            vim.keymap.set({"n", "t"}, "<c-j>", "<Cmd>FloatermNext<cr>", {buffer = event.buf})
+            vim.keymap.set({"n", "t"}, "<c-k>", "<Cmd>FloatermPrev<cr>", {buffer = event.buf})
+          end
+        '';
+      };
+    }
+  ];
+
   keymaps = [
     {
       mode = "n";
       key = "<leader>st";
       options.desc = "";
       action = ":FloatermToggle<cr>";
-    }
-    {
-      mode = "t";
-      key = "<c-h>";
-      options.desc = "";
-      action = "<Cmd>:FloatermHide<cr>";
-    }
-    {
-      mode = "t";
-      key = "<c-w>";
-      options.desc = "";
-      action = "<Cmd>:FloatermKill<cr>";
-    }
-    {
-      mode = "t";
-      key = "<c-t>";
-      options.desc = "";
-      action = "<Cmd>:FloatermNew<cr>";
-    }
-    {
-      mode = "t";
-      key = "<c-j>";
-      options.desc = "";
-      action = "<Cmd>:FloatermNext<cr>";
-    }
-    {
-      mode = "t";
-      key = "<c-k>";
-      options.desc = "";
-      action = "<Cmd>:FloatermPrev<cr>";
     }
   ];
 }
