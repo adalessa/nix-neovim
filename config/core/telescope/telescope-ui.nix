2c89784b8b4a28d902546286ef6defba928afb3c
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-01-29 20:41:27 +0100

    feat: update telescope-ui

--- config/core/telescope/telescope-ui.nix
+++ config/core/telescope/telescope-ui.nix
@@ -1,10 +1,10 @@
-{pkgs, ...}: {
-  plugins.telescope.extensionConfig = {
-    "ui-select" = {
-      __raw = "require('telescope.themes').get_dropdown({})";
+{
+  plugins.telescope = {
+    extensions.ui-select.enable = true;
+    extensionConfig = {
+      "ui-select" = {
+        __raw = "require('telescope.themes').get_dropdown({})";
+      };
     };
   };
-
-  plugins.telescope.enabledExtensions = ["ui-select"];
-  extraPlugins = with pkgs.vimPlugins; [telescope-ui-select-nvim];
 }
