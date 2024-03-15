b4badd9ffabd8ce9274da662c0c98de8bc394657
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-03-15 19:38:27 +0100

    notify: add keymap to dismiss

--- config/modules/ui/default.nix
+++ config/modules/ui/default.nix
@@ -8,6 +8,20 @@
     backgroundColour = "#000000";
   };
 
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>q";
+      options.desc = "Dismiss notifications";
+      lua = true;
+      action = ''
+        function()
+          require('notify').dismiss()
+        end
+      '';
+    }
+  ];
+
   extraPlugins = with pkgs.vimPlugins; [
     nvim-web-devicons
   ];
