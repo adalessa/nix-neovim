19917c9a8c19dca2b5667ab4ade0831799e1bfa4
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2024-03-28 11:30:08 +0100

    ui: use rounded border

--- config/modules/ui/borders.nix
+++ config/modules/ui/borders.nix
@@ -1,21 +1,10 @@
-{helpers, ...}: let
-  borders = helpers.toLuaObject [
-    ["🭽" "FloatBorder"]
-    ["▔" "FloatBorder"]
-    ["🭾" "FloatBorder"]
-    ["▕" "FloatBorder"]
-    ["🭿" "FloatBorder"]
-    ["▁" "FloatBorder"]
-    ["🭼" "FloatBorder"]
-    ["▏" "FloatBorder"]
-  ];
-in {
+{
   extraConfigLuaPost = ''
     do
       local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
       function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
         opts = opts or {}
-        opts.border = opts.border or ${borders}
+        opts.border = opts.border or "rounded"
 
         return orig_util_open_floating_preview(contents, syntax, opts, ...)
       end
