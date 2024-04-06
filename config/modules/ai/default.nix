aec2a81e61f3e831bbca82897f506e21141408e7
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-04-06 12:28:31 +0200

    Add status for ai

--- config/modules/ai/default.nix
+++ config/modules/ai/default.nix
@@ -3,4 +3,28 @@
     enable = true;
     url = "http://10.27.22.101:11434";
   };
+  extraConfigLua = ''
+    function ollama_lualine_status()
+      local status = require("ollama").status()
+      local loading = {".  ", ".. ", "..."}
+
+      if status == "IDLE" then
+        return "󰒲  "
+      elseif status == "WORKING" then
+        return loading[math.random(#loading) % #loading + 1]
+      end
+    end
+  '';
+
+  plugins.lualine.sections.lualine_y = [
+    {
+      name = "ollama_lualine_status()";
+      icon = {
+        icon = "󰚩 ";
+        color = {
+          fg = "ffffff";
+        };
+      };
+    }
+  ];
 }
