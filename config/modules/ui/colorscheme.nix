473c5ee77fcb1014e5bc2cdcde3bdce5876ad4ef
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2024-05-10 14:22:34 +0200

    fix: highlight for neogit

--- config/modules/ui/colorscheme.nix
+++ config/modules/ui/colorscheme.nix
@@ -15,4 +15,11 @@ in {
   extraConfigLuaPost = ''
     vim.cmd.colorscheme "oldworld"
   '';
+
+  highlightOverride = {
+    # NeogitDiffAddHighlight xxx cterm= gui= guifg=#90b99f guibg=#90b99f
+    NeogitDiffAddHighlight = {
+      fg = "#8d84aa";
+    };
+  };
 }
