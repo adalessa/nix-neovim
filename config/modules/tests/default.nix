132ff1778cd692e60df44b7ddb1cdd6255712bc2
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2024-07-01 20:06:38 +0200

    update the test for pest

--- config/modules/tests/default.nix
+++ config/modules/tests/default.nix
@@ -2,10 +2,11 @@
   extraPlugins = with pkgs.vimPlugins; [
     hydra-nvim
     vim-test
+    vim-dispatch
   ];
 
   globals = {
-    "test#strategy" = "neovim";
+    "test#strategy" = "dispatch";
     "test#neovim#term_position" = "vert";
     "test#php#behat#executable" = "bin/behat";
     "test#php#behat#options" = "--colors";
@@ -17,4 +18,32 @@
       ${builtins.readFile ./test.lua}
     end
   '';
+
+  extraConfigVim = ''
+    let g:dispatch_compilers = {
+      \ './vendor/bin/pest': 'pest',
+      \}
+  '';
+
+  extraFiles = {
+    "compiler/pest.vim" = ''
+      if exists('current_compiler')
+        finish
+      endif
+      let g:current_compiler = 'pest'
+
+      if exists(':CompilerSet') != 2  " older Vim always used :setlocal
+        command -nargs=* CompilerSet setlocal <args>
+      endif
+
+      let s:cpo_save = &cpoptions
+      set cpoptions-=C
+
+      CompilerSet makeprg=pest
+      CompilerSet errorformat=\ \ at\ %f:%l
+
+      let &cpoptions = s:cpo_save
+      unlet s:cpo_save
+    '';
+  };
 }
