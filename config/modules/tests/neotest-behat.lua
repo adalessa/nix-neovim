1494914f468c2c42bdd47307f223c5cb0daae6c5
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2024-08-16 16:42:59 +0200

    I think ready neotest-behat

--- config/modules/tests/neotest-behat.lua
+++ config/modules/tests/neotest-behat.lua
@@ -41,8 +41,6 @@ function NeotestAdapter.discover_positions(path)
     return nil
   end
 
-  -- TODO: parse with regular expression, get list of elements
-
   local lines = require("neotest.lib").files.read_lines(path)
   local scenarios = {}
   local feature = vim.trim(lines[1]:gsub("Feature:", ""))
@@ -94,12 +92,104 @@ end
 ---@param tree neotest.Tree
 ---@return table<string, neotest.Result>
 function NeotestAdapter.results(spec, result, tree)
-  return {
-    [tree:data().id] = {
-      status = result.code == 0 and types.ResultStatus.passed or types.ResultStatus.failed,
-      output = result.output,
-    },
-  }
+  if tree:data().type == "test" then
+    return {
+      [tree:data().id] = {
+        status = result.code == 0 and types.ResultStatus.passed or types.ResultStatus.failed,
+        output = result.output,
+      },
+    }
+  end
+
+  if tree:data().type == "file" then
+    local content = require("neotest.lib").files.read(result.output)
+    local pattern = "\n%s+tests/.-%.feature:%d+"
+
+    local failed = {}
+    for match in content:gmatch(pattern) do
+      table.insert(failed, tonumber(vim.split(match, ":")[2]))
+    end
+
+    local results = {}
+
+    for _, pos in ipairs(tree:children()) do
+      local status = types.ResultStatus.passed
+      if vim.tbl_contains(failed, pos:data().range[1] + 1) then
+        status = types.ResultStatus.failed
+      end
+
+      results[pos:data().id] = {
+        status = status,
+        output = result.output,
+      }
+    end
+
+    return results
+  end
+
+  if tree:data().type == "dir" then
+    local content = require("neotest.lib").files.read(result.output)
+    local pattern = "\n%s+tests/.-%.feature:%d+"
+
+    local failed = {}
+    for match in content:gmatch(pattern) do
+      local trimmed_match = match:match("^%s*(.-)%s*$")
+      local failed_parts = vim.split(trimmed_match, ":")
+      if failed[failed_parts[1]] == nil then
+        failed[failed_parts[1]] = {}
+      end
+
+      table.insert(failed[failed_parts[1]], tonumber(failed_parts[2]))
+    end
+
+    local results = {}
+
+    local check_per_test = function(pos)
+      local status = types.ResultStatus.passed
+
+      local file_errors = {}
+      for file, errors in pairs(failed) do
+        if vim.endswith(pos:data().path, file) then
+          file_errors = errors
+          break
+        end
+      end
+
+      if vim.tbl_contains(file_errors, pos:data().range[1] + 1) then
+        status = types.ResultStatus.failed
+      end
+
+      results[pos:data().id] = {
+        status = status,
+        output = result.output,
+      }
+    end
+
+    local check_per_file = function(pos)
+      for _, child in ipairs(pos:children()) do
+        check_per_test(child)
+      end
+    end
+
+    local check_per_dir = nil
+
+    check_per_dir = function(pos)
+      for _, child in ipairs(pos:children()) do
+        if child:data().type == "file" then
+          check_per_file(child)
+        end
+        if child:data().type == "dir" then
+          check_per_dir(child)
+        end
+      end
+    end
+
+    check_per_dir(tree)
+
+    return results
+  end
+
+  return {}
 end
 
 return NeotestAdapter
